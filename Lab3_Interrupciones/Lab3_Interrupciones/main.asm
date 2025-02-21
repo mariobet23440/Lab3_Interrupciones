2043b9dd65186d100708e0cc6cf128b1445bb3a5
mariobet23440 <bet23440@uvg.edu.gt>
2025-02-21 05:04:00 -0600

    Acoplamiento de código para display de 7 segmentos

--- Lab3_Interrupciones/Lab3_Interrupciones/main.asm
+++ Lab3_Interrupciones/Lab3_Interrupciones/main.asm
@@ -5,6 +5,7 @@
 ; Author : Mario Alejandro Betancourt Franco
 ;
 
+
 ; Encabezado
 .include "M328PDEF.inc"
 .cseg
@@ -13,9 +14,29 @@ JMP     START
 .org    PCI0addr
 JMP     ISR_PCINT0
 
+
+
 ; Definiciones
 .equ BTN_INC = PB1
 .equ BTN_DEC = PB0
+
+// Lookup Table para Display de 7 Segmentos
+.equ SEVENSD0 =	0b0111_0111
+.equ SEVENSD1 =	0b0100_0001
+.equ SEVENSD2 =	0b0011_1011
+.equ SEVENSD3 =	0b0110_1011
+.equ SEVENSD4 =	0b0100_1101
+.equ SEVENSD5 =	0b0110_1110
+.equ SEVENSD6 =	0b0111_1110
+.equ SEVENSD7 =	0b0100_0011
+.equ SEVENSD8 =	0b0111_1111
+.equ SEVENSD9 =	0b0100_1111
+.equ SEVENSDA =	0b0101_1111
+.equ SEVENSDB =	0b0111_1100
+.equ SEVENSDC =	0b0011_0110
+.equ SEVENSDD =	0b0111_1001
+.equ SEVENSDE =	0b0011_1110
+.equ SEVENSDF =	0b0001_1110 
 
 ; Variables (Estos registros NO se tocan en MAINLOOP)
 .def COUNTER = R17
@@ -78,3 +99,89 @@ DECREMENTO:
     DEC     COUNTER
     ANDI    COUNTER, 0x0F   ; Aplicar una máscara para truncar el contador a 4 bits
     RET                      ; Retornar a ISR_PCINT0
+
+
+/*
+; Definiciones
+.include "m328Pdef.inc"
+
+.equ BTN_INC = PB0       ; Botón de incremento
+.equ BTN_DEC = PB1       ; Botón de decremento
+.equ DISP = PORTD        ; Puerto del display
+
+; Lookup Table para Display de 7 Segmentos (cátodo común)
+.equ SEVENSD0 = 0b01110111
+.equ SEVENSD1 = 0b01000001
+.equ SEVENSD2 = 0b00111011
+.equ SEVENSD3 = 0b01101011
+.equ SEVENSD4 = 0b01001101
+.equ SEVENSD5 = 0b01101110
+.equ SEVENSD6 = 0b01111110
+.equ SEVENSD7 = 0b01000011
+.equ SEVENSD8 = 0b01111111
+.equ SEVENSD9 = 0b01001111
+
+; ==========================
+; SECCIÓN DE CÓDIGO PRINCIPAL
+; ==========================
+
+.cseg
+.org 0x00
+    rjmp RESET
+
+RESET:
+    ldi r16, 0xFF
+    out DDRD, r16         ; Configura PORTD como salida (para el display)
+    
+    ldi r16, 0x03
+    out PORTB, r16        ; Activa pull-ups en PB0 y PB1
+
+    clr r17               ; Inicializa contador en 0
+    rjmp MAIN_LOOP
+
+MAIN_LOOP:
+    sbic PINB, BTN_INC    ; Si el botón de incremento está presionado
+    rjmp INC_COUNT
+
+    sbic PINB, BTN_DEC    ; Si el botón de decremento está presionado
+    rjmp DEC_COUNT
+
+    rjmp MAIN_LOOP        ; Repite el bucle
+
+INC_COUNT:
+    cpi r17, 9            ; Si ya es 9, no incrementa
+    breq MAIN_LOOP
+    inc r17               ; Incrementa el contador
+    rjmp UPDATE_DISPLAY
+
+DEC_COUNT:
+    cpi r17, 0            ; Si ya es 0, no decrementa
+    breq MAIN_LOOP
+    dec r17               ; Decrementa el contador
+
+UPDATE_DISPLAY:
+    ldi ZH, high(TABLE * 2)  ; La tabla está en memoria de programa (se accede en palabras)
+    ldi ZL, low(TABLE * 2)
+    add ZL, r17               ; Apunta al índice correcto
+    lpm r16, Z                ; Carga el valor del display
+    out DISP, r16             ; Muestra el número
+    rjmp MAIN_LOOP
+
+; ==========================
+; SECCIÓN DE TABLA EN MEMORIA DE PROGRAMA
+; ==========================
+
+.org 0x100  ; Dirección alineada correctamente en memoria de programa
+TABLE:
+    .dw SEVENSD0
+    .dw SEVENSD1
+    .dw SEVENSD2
+    .dw SEVENSD3
+    .dw SEVENSD4
+    .dw SEVENSD5
+    .dw SEVENSD6
+    .dw SEVENSD7
+    .dw SEVENSD8
+    .dw SEVENSD9
+
+*/
