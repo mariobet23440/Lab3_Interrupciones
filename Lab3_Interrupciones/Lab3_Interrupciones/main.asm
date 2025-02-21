fd6df66a499e5c0a72f2d0b7cba3c65251f04eeb
mariobet23440 <bet23440@uvg.edu.gt>
2025-02-21 01:20:17 -0600

    ENTREGA DE PRELAB 1

--- Lab3_Interrupciones/Lab3_Interrupciones/main.asm
+++ Lab3_Interrupciones/Lab3_Interrupciones/main.asm
@@ -5,38 +5,41 @@
 ; Author : Mario Alejandro Betancourt Franco
 ;
 
-// Encabezado
+; Encabezado
 .include "M328PDEF.inc"
 .cseg
 .org    0x0000
+JMP     START
+.org    PCI0addr
+JMP     ISR_PCINT0
 
-// Definiciones
+; Definiciones
 .equ BTN_INC = PB1
 .equ BTN_DEC = PB0
 
-// Variables (Estos registros NO se tocan en MAINLOOP)
-.def COUNTER = R16
+; Variables (Estos registros NO se tocan en MAINLOOP)
+.def COUNTER = R17
 
-// Configurar la pila
+START:
+
+; Configurar la pila
 LDI     R16, LOW(RAMEND)
 OUT     SPL, R16
 LDI     R16, HIGH(RAMEND)
 OUT     SPH, R16
 
 SETUP:
-    // Activación de pines de entrada en el puerto B
-    LDI     R16, 0x00
-    OUT     DDRB, R16
+    ; Activación de pines de entrada en el puerto B
     LDI     R16, 0xFF            ; Habilitar pull-ups internos
     OUT     PORTB, R16
 
-    // Activación de pines de salida en el puerto C
+    ; Activación de pines de salida en el puerto C
     LDI     R16, 0xFF
     OUT     DDRC, R16
     LDI     R16, 0x00
     OUT     PORTC, R16
     
-    // Configuración de Interrupciones
+    ; Configuración de Interrupciones
     LDI     R16, (1 << PCIE0)    ; Habilitar interrupciones pin-change en PORTB
     STS     PCICR, R16
 
@@ -49,25 +52,29 @@ MAINLOOP:
     OUT     PORTC, COUNTER
     RJMP    MAINLOOP
 
-// Rutinas de Interrupción
+; Rutina de Interrupción PCINT0
 ISR_PCINT0:
-    SBI     PCIFR, PCIF0  ; Limpiar el flag de interrupción
-    IN      R17, PINB
-    
-    SBIS    PINB, BTN_INC  ; Si BTN_INC está en 0 (presionado), salta a INCREMENTO
-    RJMP    INCREMENTO
-    
-    SBIS    PINB, BTN_DEC  ; Si BTN_DEC está en 0 (presionado), salta a DECREMENTO
-    RJMP    DECREMENTO
+    PUSH    R16              ; Guardar el contenido de R16 en la pila
+    IN      R16, PINB        ; Leer el estado del puerto B
 
-    RETI
+    SBRS    R16, BTN_INC     ; Si PB1 está en LOW, incrementar
+    RCALL   INCREMENTO
 
+    SBRS    R16, BTN_DEC     ; Si PB0 está en LOW, decrementar
+    RCALL   DECREMENTO
+
+ISR_END:
+    POP     R16              ; Recuperar R16 antes de salir
+    RETI                     ; Salir de la interrupción
+
+; Incrementar el contador
 INCREMENTO:
     INC     COUNTER
     ANDI    COUNTER, 0x0F   ; Aplicar una máscara para truncar el contador a 4 bits
-    RETI
+    RET                      ; Retornar a ISR_PCINT0
 
+; Decrementar el contador
 DECREMENTO:
     DEC     COUNTER
     ANDI    COUNTER, 0x0F   ; Aplicar una máscara para truncar el contador a 4 bits
-    RETI
+    RET                      ; Retornar a ISR_PCINT0
